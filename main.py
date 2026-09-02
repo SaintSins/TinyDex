30f2477aa826446f189d45c9458717939f48e003
SaintSins <107116997+SaintSins@users.noreply.github.com>
2026-09-02 23:47:26 +0530

    feat: implement verbose output argument

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from openai import OpenAI
-import argparse
+
 
 def main() -> None:
 
@@ -14,8 +16,10 @@ def main() -> None:
     #Initialize the CLI parser and define its description
     parser = argparse.ArgumentParser(description='TinyDex')
     parser.add_argument('user_prompt', type=str, help='User prompt') #Define required positional argument
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose ouput') #Define optional argument; store_true will parse this as boolean value of True if flag is set otherwise False
     args = parser.parse_args() #Processes CLI input and packages them into 'args' obj
     prompt = args.user_prompt #Access the input via its attribute name
+    verbose = args.verbose #Access the boolean value via its attribute name
 
     #Creates a client obj
     client = OpenAI(
@@ -25,20 +29,23 @@ def main() -> None:
 
     #List of messages with role and content to build a conversation history
     messages = [{"role": "user", "content": prompt}]
-    #Calls function to generate the response by passing the messages and client obj
-    generate_content(client, messages)
+
+    #Calls function to generate the response by passing the messages, client obj, verbose flag
+    generate_content(client, messages, verbose)
 
 #Function to generate the response
-def generate_content(client:OpenAI, messages: list) -> None:
+def generate_content(client:OpenAI, messages: list, verbose:bool = False) -> None:
     #Creates a response obj
     response = client.chat.completions.create(
             model="openrouter/free",
             messages=messages
         )
-    #Checks for usage property in response obj and returns prompt_tokens and completion_tokens
-    if response.usage is None:
-        raise RuntimeError("Failed to fetch response.usage")
-    print(f'Prompt tokens: {response.usage.prompt_tokens}\nResponse tokens: {response.usage.completion_tokens}')
+    #Checks the flag for enabling verbose output
+    if verbose:
+        #Checks for usage property in response obj
+        if response.usage is None:
+            raise RuntimeError("Failed to fetch response.usage")
+        print(f'User prompt: {messages[0]["content"]}\nPrompt tokens: {response.usage.prompt_tokens}\nResponse tokens: {response.usage.completion_tokens}') #Prints user_prompt, prompt_token and completion_token
     
     #Prints the exact answer to prompt
     print(response.choices[0].message.content)
